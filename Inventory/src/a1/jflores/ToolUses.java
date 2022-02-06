8470c8bcc5b70fe527c20e9e2a9ad064c93ae364
jflores <jflores175@csub.edu>
2022-02-05 17:25:35 -0800

    a1 done

---
+++ Inventory/src/a1/jflores/ToolUses.java
@@ -0,0 +1,11 @@
+package a1.jflores;
+
+public enum ToolUses {
+    HitThings,
+    ScrewThings,
+    WrentchThings,
+    CutThings,
+    ChopThings,
+    PickThings,
+    DigThings
+}
---
+++ Inventory/src/a1/jflores/Tools.java
@@ -0,0 +1,11 @@
+package a1.jflores;
+
+public enum Tools {
+    Hammer,
+    Screwdriver,
+    Wrench,
+    Saw,
+    Axe,
+    Pickaxe,
+    Shovel
+}
