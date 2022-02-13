0da8cac199b3ab82be6c868a94a0ce5c3ad876ce
jflores <jflores175@csub.edu>
2022-02-12 19:26:50 -0800

    Started A2

--- Inventory/src/a1/jflores/Main.java
+++ Inventory/src/a1/jflores/Main.java
@@ -6,17 +6,23 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
+    private static final Random ran = new Random();
+    private static final FoodItems[] foodItems = FoodItems.values();
+    private static final Tools[] tools = Tools.values();
+    private static final ToolUses[] toolUses = ToolUses.values();
+    private static final Clothes[] clothes = Clothes.values();
+    private static final Weapons[] weapons = Weapons.values();
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Random ran = new Random();
+        //Random ran = new Random();
         List<Item> items = new ArrayList<>();
 
-        FoodItems[] foodItems = FoodItems.values();
-        Tools[] tools = Tools.values();
-        ToolUses[] toolUses = ToolUses.values();
-        Clothes[] clothes = Clothes.values();
-        Weapons[] weapons = Weapons.values();
+        //FoodItems[] foodItems = FoodItems.values();
+        //Tools[] tools = Tools.values();
+        //ToolUses[] toolUses = ToolUses.values();
+        //Clothes[] clothes = Clothes.values();
+        //Weapons[] weapons = Weapons.values();
 
         System.out.print("How many items do you want: ");
         int itemCnt = Integer.parseInt(scan.nextLine());
@@ -24,43 +30,10 @@ public class Main {
         for (int i = 0; i <itemCnt; i++) {
             int type = ran.nextInt(4);
             switch (type) {
-                case 0 -> {
-                    int foodIndex = ran.nextInt(foodItems.length);
-                    String foodName = foodItems[foodIndex].toString();
-                    float foodPrice = ran.nextFloat(10);
-                    int foodQty = ran.nextInt(30);
-                    int foodUses = ran.nextInt(6);
-                    float healthGain = ran.nextFloat(20);
-                    Food tmpFood = new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
-                    items.add(tmpFood);
-                }
-                case 1 -> {
-                    int toolIndex = ran.nextInt(tools.length);
-                    String toolName = tools[toolIndex].toString();
-                    float toolPrice = ran.nextFloat(200);
-                    int toolQty = ran.nextInt(15);
-                    String use = toolUses[toolIndex].toString();
-                    Tool tmpTool = new Tool(toolName, toolPrice, toolQty, use);
-                    items.add(tmpTool);
-                }
-                case 2 -> {
-                    int clothingIndex = ran.nextInt(clothes.length);
-                    String clothingName = clothes[clothingIndex].toString();
-                    float clothingPrice = ran.nextFloat(100);
-                    int clothingQty = ran.nextInt(20);
-                    float clothingDef = ran.nextFloat(20);
-                    Clothing tmpClothing = new Clothing(clothingName, clothingPrice, clothingQty, clothingDef);
-                    items.add(tmpClothing);
-                }
-                case 3 -> {
-                    int weaponIndex = ran.nextInt(weapons.length);
-                    String weaponName = weapons[weaponIndex].toString();
-                    float weaponPrice = ran.nextFloat(100);
-                    int weaponQty = ran.nextInt(15);
-                    float weaponAtk = ran.nextFloat(50);
-                    Weapon tmpWeapon = new Weapon(weaponName, weaponPrice, weaponQty, weaponAtk);
-                    items.add(tmpWeapon);
-                }
+                case 0 -> items.add(genFood());
+                case 1 -> items.add(genTool());
+                case 2 -> items.add(genClothing());
+                case 3 -> items.add(genWeapon());
             }
         }
 
@@ -69,6 +42,51 @@ public class Main {
         for(Item i : items) {
             System.out.println(i);
         }
+    }
+
+    public static Food genFood() {
+        int foodIndex = ran.nextInt(foodItems.length);
+        String foodName = foodItems[foodIndex].toString();
+        float foodPrice = ran.nextFloat(10);
+        int foodQty = ran.nextInt(30);
+        int foodUses = ran.nextInt(6);
+        float healthGain = ran.nextFloat(20);
+        return new Food(foodName, foodPrice, foodQty, foodUses, healthGain);
+        //items.add(tmpFood);
+        //return tmpFood;
+    }
+
+    public static Tool genTool() {
+        int toolIndex = ran.nextInt(tools.length);
+        String toolName = tools[toolIndex].toString();
+        float toolPrice = ran.nextFloat(200);
+        int toolQty = ran.nextInt(15);
+        String use = toolUses[toolIndex].toString();
+        return new Tool(toolName, toolPrice, toolQty, use);
+        //items.add(tmpTool);
+        //return tmpTool;
+    }
+
+    public static Clothing genClothing() {
+        int clothingIndex = ran.nextInt(clothes.length);
+        String clothingName = clothes[clothingIndex].toString();
+        float clothingPrice = ran.nextFloat(100);
+        int clothingQty = ran.nextInt(20);
+        float clothingDef = ran.nextFloat(20);
+        return new Clothing(clothingName, clothingPrice, clothingQty, clothingDef);
+        //items.add(tmpClothing);
+        //return tmpClothing;
+    }
+
+    public static Weapon genWeapon() {
+        int weaponIndex = ran.nextInt(weapons.length);
+        String weaponName = weapons[weaponIndex].toString();
+        float weaponPrice = ran.nextFloat(100);
+        int weaponQty = ran.nextInt(15);
+        float weaponAtk = ran.nextFloat(50);
+        return new Weapon(weaponName, weaponPrice, weaponQty, weaponAtk);
+        //items.add(tmpWeapon);
+        //return tmpWeapon;
     }
 }
 
---
+++ MultiThreadSort/src/a2/jflores/Main.java
@@ -0,0 +1,12 @@
+package a2.jflores;
+
+import a1.jflores.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Food f = a1.jflores.Main.genFood();
+        System.out.println(f);
+    }
+}
