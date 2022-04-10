258522cc2fd0b5abbd222beae16650429899560e
jflores <jflores175@csub.edu>
2022-04-09 20:59:59 -0700

    a9 Done

---
+++ MobileTodo/app/src/main/java/a9/jflores/mobiletodo/Item.java
@@ -0,0 +1,18 @@
+package a9.jflores.mobiletodo;
+
+public class Item {
+    private final String desc;
+
+    public Item(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return this.getDesc();
+    }
+}
